144d7597b30781633f668ebd1042adbb4947d933
Iti Shree <itishree1999@gmail.com>
2018-07-30 01:35:06 +0530

    Re-written code
    Updated the whole code to meet the requirements of calculation of raw histogram value and write it into text file. You can plot graph using the generated text file.

--- Histogram/hist.c
+++ Histogram/hist.c
@@ -1,212 +1,198 @@
-// Copyright(C) 2018 Iti Shree
+/**********************************************************************
+**  histogram.c
+**
+**	Calculating frequency of brightness level of raw data.
+**
+**	Version 1.0
+**
+**  Copyright (C) 2018 Iti Shree
+**
+**	This program is free software: you can redistribute it and/or
+**	modify it under the terms of the GNU General Public License
+**	as published by the Free Software Foundation, either version
+**	2 of the License, or (at your option) any later version.
+**
+**********************************************************************/
 
-
+#include <errno.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <unistd.h>
 #include <string.h>
-#include <errno.h>
+#include <unistd.h>
 
-#include <sys/mman.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
-/* Constant declaration */
-#define MAX_IMAGESIZE  1024
-#define MAX_BRIGHTNESS  255 /* Maximum gray level */
-#define GRAYLEVEL       256 /* No. of gray levels */
-#define MAX_FILENAME    256 /* Filename length limit */
-#define MAX_BUFFERSIZE  256
+static char *cmd_name = NULL;
 
-/* Image storage arrays */
-unsigned char **image1 = NULL;
-unsigned char **image2 = NULL;
-unsigned char **imageCropped = NULL;
-int x_size1, y_size1, /* width & height of image1*/
-        x_size2, y_size2; /* width & height of image2 */
+static uint32_t map_base = 0x18000000;
+static uint32_t map_size = 0x08000000;
+static uint32_t map_addr = 0x00000000;
 
-/* Prototype declaration of functions */
-void load_image_file(); /* image input   */
-void save_image_file(); /* image output*/
-void crop_image_file(); /* crops image */
+static char *dev_mem = "/dev/mem";
 
-void load_image_file(char *filename)
-/* Input of header & body information of pgm file */
-/* for image1[ ][ ]，x_size1，y_size1 */
+#define NUM_COLS 4096
+#define NUM_ROWS 3072
+
+void write_data(FILE *fp, uint8_t *hist)
 {
-    char *buffer = (char *) malloc(MAX_IMAGESIZE * sizeof(char));
-    FILE *fp; /* File pointer */
-    int max_gray; /* Maximum gray level */
-    int x, y; /* Loop variable */
+  if (fp == NULL)
+  {
+    printf("Error while opening the file.\n");
+    exit(0);
+  }
 
-    /* Input file open */
-    fp = fopen(filename, "rb");
-    if (NULL == fp) {
-        printf("     The file doesn't exist!\n\n");
-        exit(1);
-    }
-    /* input of x_size1, y_size1 */
-    x_size1 = 0;
-    y_size1 = 0;
-    while (x_size1 == 0 || y_size1 == 0) {
-        fgets(buffer, MAX_BUFFERSIZE, fp);
-        if (buffer[0] != '#') {
-            sscanf(buffer, "%d %d", &x_size1, &y_size1);
-        }
-    }
-    /* input of max_gray */
-    max_gray = 0;
-    while (max_gray == 0) {
-        fgets(buffer, MAX_BUFFERSIZE, fp);
-        if (buffer[0] != '#') {
-            sscanf(buffer, "%d", &max_gray);
-        }
-    }
-    if (x_size1 > MAX_IMAGESIZE || y_size1 > MAX_IMAGESIZE) {
-        printf("     Image size exceeds %d x %d\n\n",
-               MAX_IMAGESIZE, MAX_IMAGESIZE);
-        printf("     Please use smaller images!\n\n");
-        exit(1);
-    }
-    if (max_gray != MAX_BRIGHTNESS) {
-        printf("     Invalid value of maximum gray level!\n\n");
-        exit(1);
-    }
-    /* Input of image data*/
-
-    image1 = (unsigned char *) malloc(MAX_IMAGESIZE * MAX_IMAGESIZE * sizeof(unsigned char));
-
-    for (y = 0; y < y_size1; y++) {
-        for (x = 0; x < x_size1; x++) {
-            image1[y][x] = (unsigned char) fgetc(fp);
-        }
-    }
-    free(buffer);
-    fclose(fp);
+  for (unsigned i = 0; i < 256; i++)
+    fprintf(fp, "%u\n", hist[i]);
+  fclose(fp);
 }
 
-void crop_image_file(char *filename)
-/* Output cropped imageCropped[][], x_cropped, y_cropped */
+void hist_calc(uint8_t *buf, uint8_t *hist)
 {
-    FILE *fp;
-    int i, j;
-    int x1, x2, y1, y2;
 
-    imageCropped = (unsigned char *) malloc(MAX_IMAGESIZE * MAX_IMAGESIZE * sizeof(unsigned char));
+  for (unsigned j = 0; j < NUM_COLS / 2; j++)
+  {
+    unsigned ce = j * 3;
+    unsigned co = (j + NUM_COLS / 2) * 3;
 
-    /* Getting coordinates required for cropped images */
+    hist[buf[ce + 0]]++;
+    hist[buf[ce + 1]]++;
+    hist[buf[ce + 2]]++;
 
-    scanf("%d %d", &x1, &x2);
-    scanf("%d %d", &y1, &y2);
-
-    for (i = 0; i < x_size1; i++) {
-        for (j = 0; j < y_size1; j++) {
-            imageCropped[i][j] = 0;
-        }
-    }
-
-    for (i = 0; i < x_size1; i++) {
-        for (j = 0; j < y_size1; j++) {
-            if ((i >= x1 && i <= x2) && (j >= y1 && j <= y2))
-                imageCropped[i][j] = image1[i][j];
-        }
-    }
-
+    hist[buf[co + 0]]++;
+    hist[buf[co + 1]]++;
+    hist[buf[co + 2]]++;
+  }
 }
 
-void save_image_file(char *filename)
-/* Output of image2[ ][ ], x_size2, y_size2 */
+void load_data(uint8_t *buf, uint64_t *ptr)
 {
-    FILE *fp; /* File pointer */
-    int x, y; /* Loop variable */
 
-    image2 = (unsigned char *) malloc(MAX_IMAGESIZE * MAX_IMAGESIZE * sizeof(unsigned char));
+  for (unsigned i = 0; i < NUM_COLS / 2; i++)
+  {
+    unsigned ce = i * 3;
+    unsigned co = (i + NUM_COLS / 2) * 3;
 
-    fp = fopen(filename, "wb");
-    /* output of file header information */
-    fputs("P5\n", fp);
-    fputs("# Created by Image Processing\n", fp);
-    fprintf(fp, "%d %d\n", x_size2, y_size2);
-    fprintf(fp, "%d\n", MAX_BRIGHTNESS);
-    /* Output of image data */
-    for (y = 0; y < y_size2; y++) {
-        for (x = 0; x < x_size2; x++) {
-            fputc(image2[y][x], fp);
-        }
-    }
-    fclose(fp);
+    uint64_t val = ptr[i];
+
+    buf[ce + 0] = (val >> 56) & 0xFF;
+    buf[ce + 1] = (val >> 48) & 0xFF;
+    buf[ce + 2] = (val >> 40) & 0xFF;
+
+    buf[co + 0] = (val >> 32) & 0xFF;
+    buf[co + 1] = (val >> 24) & 0xFF;
+    buf[co + 2] = (val >> 16) & 0xFF;
+  }
 }
 
-void make_histogram_image()
-/* Histogram of image1 is output into image2 */
+int main(int argc, char **argv)
 {
-    long int *histogram = (long int *) malloc(GRAYLEVEL * sizeof(long int));
-    long int max_frequency; /* Maximum frequency */
-    int i, j, x, y; /* control variable */
-    int height; /* height of histogram */
-    int top; /* the most frequent graylevel */
+  extern int optind;
+  extern char *optarg;
+  int c, err_flag = 0;
 
-    image2 = (unsigned char *) malloc(MAX_IMAGESIZE * MAX_IMAGESIZE * sizeof(unsigned char));
+#define OPTIONS "hB:S:"
+#define VERSION "1.0"
 
-    /* Calculation of histogram */
-    for (i = 0; i < GRAYLEVEL; i++) {
-        histogram[i] = 0;
+  cmd_name = argv[0];
+  while ((c = getopt(argc, argv, OPTIONS)) != EOF)
+  {
+    switch (c)
+    {
+    case 'h':
+      fprintf(stderr, "This is %s " VERSION "\n"
+                      "options are:\n"
+                      "-h        print this help message\n"
+                      "-B <val>  memory mapping base\n"
+                      "-S <val>  memory mapping size\n",
+              cmd_name);
+      exit(0);
+      break;
+
+    case 'B':
+      map_base = strtoll(optarg, NULL, 0);
+      break;
+
+    case 'S':
+      map_size = strtoll(optarg, NULL, 0);
+      break;
+
+    default:
+      err_flag++;
+      break;
     }
-    printf("Now, histogram of input image is calculated\n\n");
-    for (y = 0; y < y_size1; y++) {
-        for (x = 0; x < x_size1; x++) {
-            histogram[imageCropped[y][x]]++;
-        }
+  }
+
+  /* If no option is matched print this message */
+  if (err_flag)
+  {
+    fprintf(stderr, "Usage: %s -[" OPTIONS "] [file]\n"
+                    "%s -h for help.\n",
+            cmd_name, cmd_name);
+    exit(1);
+  }
+
+  /* Opening dev/mem with read/write permission */
+  int fd = open(dev_mem, O_RDWR | O_SYNC);
+  if (fd == -1)
+  {
+    fprintf(stderr, "error opening >%s<.\n%s\n", dev_mem, strerror(errno));
+    exit(2);
+  }
+
+  if (map_addr == 0)
+    map_addr = map_base;
+
+  /* Mapping base */
+  void *base = mmap((void *)map_addr, map_size, PROT_READ | PROT_WRITE,
+                    MAP_SHARED, fd, map_base);
+  if (base == (void *)-1)
+  {
+    fprintf(stderr, "error mapping 0x%08X+0x%08X @0x%08X.\n%s\n", map_base,
+            map_size, map_addr, strerror(errno));
+    exit(2);
+  }
+
+  fprintf(stderr, "mapped 0x%08lX+0x%08lX to 0x%08lX.\n",
+          (long unsigned)map_base, (long unsigned)map_size,
+          (long unsigned)base);
+
+  /* Checking if filename was supplied, with option to create file */
+  if (argc > optind)
+  {
+    close(1);
+    int fd = open(argv[optind], O_CREAT | O_WRONLY, S_IRUSR);
+
+    if (fd == -1)
+    {
+      fprintf(stderr, "error opening >%s< for writing.\n%s\n", argv[optind],
+              strerror(errno));
+      exit(2);
     }
-    /* calculation of maximum frequency */
-    max_frequency = histogram[0];
-    top = 0;
-    for (i = 1; i < GRAYLEVEL; i++) {
-        if (histogram[i] > max_frequency) {
-            max_frequency = histogram[i];
-            top = i;
-        }
-    }
-    printf("Maximum frequency = %ld at level of %d\n\n", max_frequency, top);
-    /* Histogram image generation */
-    x_size2 = MAX_IMAGESIZE;
-    y_size2 = MAX_IMAGESIZE;
-    for (y = 0; y < y_size2; y++) {
-        for (x = 0; x < x_size2; x++) {
-            image2[y][x] = 0;
-        }
-    }
-    for (i = 0; i < GRAYLEVEL; i++) {
-        height = (int) (MAX_BRIGHTNESS /
-                        (double) max_frequency * (double) histogram[i]);
-        for (j = 0; j < height; j++) {
-            image2[MAX_IMAGESIZE - 1 - j][i] = MAX_BRIGHTNESS;
-        }
-    }
+  }
+
+  uint8_t buf[NUM_COLS * 3];
+  uint8_t hist[256];
+
+  for (unsigned i = 0; i < 256; i++)
+    hist[i] = 0;
+
+  uint64_t *ptr = base;
+  FILE *fp;
+
+  char *fileName = "histogram.txt";
+
+  for (unsigned i = 0; i < NUM_ROWS / 2; i++)
+  {
+    load_data(buf, ptr);
+    hist_calc(buf, hist);
+    write_data(fp = fopen(fileName, "w+"), hist);
+    ptr += (NUM_COLS / 2);
+  }
+
+  exit(0);
 }
-
-int main(int argc, char *argv[]) {
-    // Checking the input
-    if (argc != 2) {
-        printf("Invalid command!\n");
-        return -1;
-    }
-    char *fName;
-    int fd = open(fName, O_RDWR | O_SYNC);
-    if (fd == -1) {
-        fprintf(stderr,
-                "error opening >%s<.\n%s\n",
-                fName, strerror(errno));
-        exit(1);
-    }
-
-    load_image_file(fName);      /* Input of image1 */
-    crop_image_file(fName);      /* Crop image1 and input the data into imageCropped */
-    make_histogram_image(fName); /* Histogram generation */
-    save_image_file(fName);      /* Output of image2 */
-    return 0;
-}
-
