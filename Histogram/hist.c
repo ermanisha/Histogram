782c530e3d4d628b52a382c74a33ef3a6543205e
Iti Shree <itishree1999@gmail.com>
2018-08-03 22:50:25 +0530

    Updated code

--- Histogram/hist.c
+++ Histogram/hist.c
@@ -38,161 +38,164 @@ static char *dev_mem = "/dev/mem";
 #define NUM_COLS 4096
 #define NUM_ROWS 3072
 
-void write_data(FILE *fp, uint8_t *hist)
+#define ERR_WRITE 1
+
+void write_data(FILE *fp, uint16_t hist[4096])
 {
-  if (fp == NULL)
-  {
-    printf("Error while opening the file.\n");
-    exit(0);
-  }
+	if (fp == NULL)
+	{
+		printf("Error while opening the file.\n");
+		exit(0);
+	}
 
-  for (unsigned i = 0; i < 256; i++)
-    fprintf(fp, "%u\n", hist[i]);
-  fclose(fp);
-}
-
-void hist_calc(uint8_t *buf, uint8_t *hist)
-{
-
-  for (unsigned j = 0; j < NUM_COLS / 2; j++)
-  {
-    unsigned ce = j * 3;
-    unsigned co = (j + NUM_COLS / 2) * 3;
-
-    hist[buf[ce + 0]]++;
-    hist[buf[ce + 1]]++;
-    hist[buf[ce + 2]]++;
-
-    hist[buf[co + 0]]++;
-    hist[buf[co + 1]]++;
-    hist[buf[co + 2]]++;
-  }
+	for (unsigned i = 0; i < 4096; i++)
+		fprintf(fp, "%u\t%u\n", i, hist[i]);
+	fclose(fp);
 }
 
 void load_data(uint8_t *buf, uint64_t *ptr)
 {
 
-  for (unsigned i = 0; i < NUM_COLS / 2; i++)
-  {
-    unsigned ce = i * 3;
-    unsigned co = (i + NUM_COLS / 2) * 3;
+	for (unsigned i = 0; i < NUM_COLS / 2; i++)
+	{
+		unsigned ce = i * 3;
+		unsigned co = (i + NUM_COLS / 2) * 3;
 
-    uint64_t val = ptr[i];
+		uint64_t val = ptr[i];
 
-    buf[ce + 0] = (val >> 56) & 0xFF;
-    buf[ce + 1] = (val >> 48) & 0xFF;
-    buf[ce + 2] = (val >> 40) & 0xFF;
+		buf[ce + 0] = (val >> 56) & 0xFF;
+		buf[ce + 1] = (val >> 48) & 0xFF;
+		buf[ce + 2] = (val >> 40) & 0xFF;
 
-    buf[co + 0] = (val >> 32) & 0xFF;
-    buf[co + 1] = (val >> 24) & 0xFF;
-    buf[co + 2] = (val >> 16) & 0xFF;
-  }
+		buf[co + 0] = (val >> 32) & 0xFF;
+		buf[co + 1] = (val >> 24) & 0xFF;
+		buf[co + 2] = (val >> 16) & 0xFF;
+	}
 }
 
 int main(int argc, char **argv)
 {
-  extern int optind;
-  extern char *optarg;
-  int c, err_flag = 0;
+	extern int optind;
+	extern char *optarg;
+	int c, err_flag = 0;
 
 #define OPTIONS "hB:S:"
 #define VERSION "1.0"
 
-  cmd_name = argv[0];
-  while ((c = getopt(argc, argv, OPTIONS)) != EOF)
-  {
-    switch (c)
-    {
-    case 'h':
-      fprintf(stderr, "This is %s " VERSION "\n"
-                      "options are:\n"
-                      "-h        print this help message\n"
-                      "-B <val>  memory mapping base\n"
-                      "-S <val>  memory mapping size\n",
-              cmd_name);
-      exit(0);
-      break;
+	cmd_name = argv[0];
+	while ((c = getopt(argc, argv, OPTIONS)) != EOF)
+	{
+		switch (c)
+		{
+		case 'h':
+			fprintf(stderr, "This is %s " VERSION "\n"
+							"options are:\n"
+							"-h        print this help message\n"
+							"-B <val>  memory mapping base\n"
+							"-S <val>  memory mapping size\n",
+					cmd_name);
+			exit(0);
+			break;
 
-    case 'B':
-      map_base = strtoll(optarg, NULL, 0);
-      break;
+		case 'B':
+			map_base = strtoll(optarg, NULL, 0);
+			break;
 
-    case 'S':
-      map_size = strtoll(optarg, NULL, 0);
-      break;
+		case 'S':
+			map_size = strtoll(optarg, NULL, 0);
+			break;
 
-    default:
-      err_flag++;
-      break;
-    }
-  }
+		default:
+			err_flag++;
+			break;
+		}
+	}
 
-  /* If no option is matched print this message */
-  if (err_flag)
-  {
-    fprintf(stderr, "Usage: %s -[" OPTIONS "] [file]\n"
-                    "%s -h for help.\n",
-            cmd_name, cmd_name);
-    exit(1);
-  }
+	if (err_flag)
+	{
+		fprintf(stderr, "Usage: %s -[" OPTIONS "] [file]\n"
+						"%s -h for help.\n",
+				cmd_name, cmd_name);
+		exit(1);
+	}
 
-  /* Opening dev/mem with read/write permission */
-  int fd = open(dev_mem, O_RDWR | O_SYNC);
-  if (fd == -1)
-  {
-    fprintf(stderr, "error opening >%s<.\n%s\n", dev_mem, strerror(errno));
-    exit(2);
-  }
+	int fd = open(dev_mem, O_RDWR | O_SYNC);
+	if (fd == -1)
+	{
+		fprintf(stderr,
+				"error opening >%s<.\n%s\n", dev_mem, strerror(errno));
+		exit(2);
+	}
 
-  if (map_addr == 0)
-    map_addr = map_base;
+	if (map_addr == 0)
+		map_addr = map_base;
 
-  /* Mapping base */
-  void *base = mmap((void *)map_addr, map_size, PROT_READ | PROT_WRITE,
-                    MAP_SHARED, fd, map_base);
-  if (base == (void *)-1)
-  {
-    fprintf(stderr, "error mapping 0x%08X+0x%08X @0x%08X.\n%s\n", map_base,
-            map_size, map_addr, strerror(errno));
-    exit(2);
-  }
+	void *base = mmap((void *)map_addr, map_size, PROT_READ | PROT_WRITE,
+					  MAP_SHARED, fd, map_base);
+	if (base == (void *)-1)
+	{
+		fprintf(stderr,
+				"error mapping 0x%08X+0x%08X @0x%08X.\n%s\n", map_base,
+				map_size, map_addr, strerror(errno));
+		exit(2);
+	}
 
-  fprintf(stderr, "mapped 0x%08lX+0x%08lX to 0x%08lX.\n",
-          (long unsigned)map_base, (long unsigned)map_size,
-          (long unsigned)base);
+	fprintf(stderr,
+			"mapped 0x%08lX+0x%08lX to 0x%08lX.\n",
+			(long unsigned)map_base, (long unsigned)map_size,
+			(long unsigned)base);
 
-  /* Checking if filename was supplied, with option to create file */
-  if (argc > optind)
-  {
-    close(1);
-    int fd = open(argv[optind], O_CREAT | O_WRONLY, S_IRUSR);
+	if (argc > optind)
+	{
+		close(0);
 
-    if (fd == -1)
-    {
-      fprintf(stderr, "error opening >%s< for writing.\n%s\n", argv[optind],
-              strerror(errno));
-      exit(2);
-    }
-  }
+		int fd = open(argv[optind], O_RDONLY, 0);
+		if (fd == -1)
+		{
+			fprintf(stderr,
+					"error opening >%s< for reading.\n%s\n", argv[optind],
+					strerror(errno));
+			exit(2);
+		}
+	}
 
-  uint8_t buf[NUM_COLS * 3];
-  uint8_t hist[256];
+	uint16_t buf[NUM_COLS * 3];
+	uint64_t *ptr = base;
 
-  for (unsigned i = 0; i < 256; i++)
-    hist[i] = 0;
+	uint16_t hist[4096] = {0};
 
-  uint64_t *ptr = base;
-  FILE *fp;
+	for (unsigned j = 0; j < NUM_ROWS / 2; j++)
+	{
+		for (unsigned i = 0; i < NUM_ROWS / 2; i++)
+		{
+			unsigned ce = i * 4;
 
-  char *fileName = "histogram.txt";
+			uint64_t val = ptr[i];
 
-  for (unsigned i = 0; i < NUM_ROWS / 2; i++)
-  {
-    load_data(buf, ptr);
-    hist_calc(buf, hist);
-    write_data(fp = fopen(fileName, "w+"), hist);
-    ptr += (NUM_COLS / 2);
-  }
+			buf[ce + 0] = (val >> 52) & 0xFFF;
+			buf[ce + 1] = (val >> 40) & 0xFFF;
+			buf[ce + 2] = (val >> 28) & 0xFFF;
+			buf[ce + 3] = (val >> 16) & 0xFFF;
+		}
 
-  exit(0);
+		for (unsigned i = 0; i < NUM_ROWS * 2; i++)
+		{
+			hist[buf[i]]++;
+		}
+
+		ptr += NUM_COLS / 2;
+	}
+
+	unsigned sum = 0;
+
+	for (unsigned i = 0; i < 10; i++)
+		sum += hist[i];
+
+	printf("%u", sum);
+
+	FILE *fp;
+	char *fileName = "sample.txt";
+	write_data(fp = fopen(fileName, "w+"), hist);
+
+	exit(0);
 }
